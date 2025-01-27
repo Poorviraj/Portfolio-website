1e187239fe28ee25c8ac821e26a93b44eb08c6e8
poorviraj <poorvirajsinghnirwan@gmail.com>
2025-01-28 00:19:29 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,76 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications.  I have honed my skills in front-end technologies like React and JS, as well as back-end technologies like Node.js, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `"Enthusiastic and dedicated full-stack web developer with a strong foundation in both front-end and back-end technologies. Eager to contribute as a fresher, leveraging my skills in HTML, CSS, JavaScript, and frameworks like React and Node.js to build dynamic and user-friendly applications. Passionate about problem-solving and continuously learning to keep up with industry trends.".`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022-2026",
+    role: "Btech (CSE)  spec : WEBDEV",
+    company: "LPU",
+    description: `"Graduated with a BTech in Computer Science and Engineering in 2026, specializing in web development. Acquired comprehensive skills in designing and building dynamic web applications, focusing on user experience and responsive design."`,
+    technologies: ["Javascript", "React.js", "Express", "mongoDB"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Freelancer",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["React", "Node", "Bootstrap", "Postgres"],
+  },
+  {
+    year: "2020-2021",
+    role: "Intermediate",
+    company: "Lamba Convent School",
+    description: ` "Graduated from Lamba Convent School in 2021, successfully completing the 12th grade with a strong focus on discipline and leadership. Developed a solid academic foundation and a commitment to serving the community, embodying the values instilled by a military education."`,
+    technologies: ["Physics", "Chemistry", "Maths", ],
+  },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["Tailwind CSS", "React", "Node.js", "MongoDB", "Express"],
+  },
+  {
+    title: "URL_Shortner",
+    image: project2,
+    description:
+      "Created a URL Shortener website with essential commerce features",
+    technologies: ["React", "Tailwind CSS", "React", "Node.js"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Question search Application",
+    image: project4,
+    description:
+      "A platform for seacrhing Keyword from a huge Database and giving you all the question who have that keyword.",
+    technologies: ["React", "Javascript", "Express", "mongodb", "Tailwind CSS"],
+  },
+];
+
+export const CONTACT = {
+  address: "Jhunjhunu , Rajasthan ,India",
+  phoneNo: "+91 8209302677 ",
+  email: "poorvirajsinghnirwan@gmail.com",
+};
